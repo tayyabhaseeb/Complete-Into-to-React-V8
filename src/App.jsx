1408d208c361b2f4f5ff359bacd2bbd7033419cd
“Tayyab <tayyabhaseeb5@gmail.com>
2024-07-02 23:30:53 +0100

    latest changes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
