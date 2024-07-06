28883c0d9aa23a449fef8db5916de468183b1d55
“Tayyab <tayyabhaseeb5@gmail.com>
2024-07-06 23:51:56 +0100

    adds react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import Pet from "./components/Pet";
 import SearchParams from "./components/SearchParams";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Details from "./components/Details";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        cacheTime: Infinity,
+      },
+    },
+  });
+
   return (
-    <div>
-      <h1>Adopt Me</h1>
-      <SearchParams />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
